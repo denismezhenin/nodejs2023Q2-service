7965c51836d5303a8e4b41ff597e0685a37f8edb
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2023-07-31 17:54:27 +0200

    fix:main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { load } from 'js-yaml';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as yaml from 'js-yaml';
 import { readFile } from 'fs/promises';
 import { ValidationPipe } from '@nestjs/common';
+import YAML from 'yamljs';
+import { readFileSync } from 'fs';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get<ConfigService>(ConfigService);
   const port = appConfig.get('PORT');
-  const file = await readFile('doc/api.yaml', 'utf8');
-  const yamlDoc = load(file);
+  const file = await readFileSync('doc/api.yaml', 'utf8');
+  const yamlDoc = yaml.load(file);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  SwaggerModule.setup('api', app, yamlDoc);
+  SwaggerModule.setup('doc', app, yamlDoc);
   await app.listen(port || 4000);
 }
 bootstrap();
